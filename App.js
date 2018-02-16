ebcfb20b58640480ff0c3430c1046b6c25e4e372
Arturo Cabellero <acaballero9085@gmail.com>
2018-02-15 20:38:29 -0600

    Landing screen is up

--- App.js
+++ App.js
@@ -1,23 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Font } from 'expo'
+import { Text, View, TouchableHighlight } from 'react-native'
+
+// Styles
+import { styles } from './AppStyles'
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      fontLoaded: false
+    }
+  }
+
+  async componentDidMount(){
+    await Font.loadAsync({
+      'comfort': require('./assets/fonts/Comfortaa-Bold.ttf')
+    })
+
+    this.setState({fontLoaded: true})
+  }
+
   render() {
+    const { fontLoaded } = this.state
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+
+      {fontLoaded ? 
+        <View>
+
+          <Text style={styles.titleText}>tic tac toe</Text>
+
+        </View> : null
+      }
+
+      {fontLoaded ? 
+        <View>
+          <TouchableHighlight 
+            style={styles.button}
+            onPress={() => alert('here')}
+            underlayColor={'#fff'}
+          >
+            <Text style={styles.buttonText}>start</Text>
+          </TouchableHighlight>
+
+          <TouchableHighlight 
+            style={styles.button}
+            onPress={() => alert('here')}
+            underlayColor={'#fff'}
+          >
+            <Text style={styles.buttonText}>about</Text>
+          </TouchableHighlight>
+        </View> : null
+      }
+        
       </View>
-    );
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ AppStyles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native'
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    titleText: {
+        fontFamily: 'comfort',
+        fontSize: 40
+    },
+    buttonText: {
+        fontFamily: 'comfort',
+        fontSize: 20
+    },
+    button: {
+        height: 30,
+        width: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 20
+    }
+})
